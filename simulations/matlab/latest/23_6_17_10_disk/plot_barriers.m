2494b3f407de522058ce5dafb878411644070cb6
kkarimi62 <kamran.karimi1362@gmail.com>
2023-06-28 11:55:11 +0200

    updates

--- simulations/matlab/latest/23_6_17_10_disk/plot_barriers.m
+++ simulations/matlab/latest/23_6_17_10_disk/plot_barriers.m
@@ -3,7 +3,7 @@ function plot_barriers()
 global   r_b c_bx c_by
 
             centers=[c_bx' c_by'];
-            viscircles(centers,r_b,Color="b");
+            viscircles(centers,r_b,'Color',"b");
             pbaspect([1 1 1]);
 
             hold on
