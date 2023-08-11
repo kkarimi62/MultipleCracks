f271663cb4f79f2ee5103055097a33aacc25f90b
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-11 18:21:28 +0200

    update

--- simulations/matlab/plot_field.m
+++ simulations/matlab/plot_field.m
@@ -107,7 +107,46 @@ figure(3)
     
     saveas(gcf,'png/sigmaxy.png')
 
+figure(4)
+    sxx = S_xx/sig_amp;
+    syy = S_yy/sig_amp;
+    sxy = S_xy/sig_amp;
+    exx = (sxx-nu*syy);
+    eyy = (syy-nu*sxx);
+    exy =  sxy;
+    p   = 0.5*(exx+eyy);
+    r2  = ((exx-eyy)*(exx-eyy)+4*exy*exy)*0.25;
+    r   = sqrt(r2);
+    e1  = p + r;
+    e2  = p - r;
+    vonMisesStrain = sqrt( e1 * e1 + e2 * e2 - e1 * e2 );
+    contourf(X,Y,vonMisesStrain,zlevs);
+    colorbar;
+    colormap(jet(256));
+    pbaspect([1 1 1]);
+    xlabel('x [b]','FontName','Times New Roman','FontSize',fsize)
+    ylabel('y [b]','FontSize',14,'FontName', 'Times New Roman')
+    set(gca,'fontsize',14,'fontname','Times New Roman')
+%     caxis([color_scale1, color_scale2]);
+     hold on
 
+    for i=1:n_c
+    n_dis=2*C(i).n_d;
+    R_Left=C(i).D(1).R_p-5*C(i).b_mag*C(i).e_p;
+    R_Right=C(i).D(n_dis).R_p+5*C(i).b_mag*C(i).e_p;
+    xx_cod_up=[R_Left(1),x_cod_up(i,1:n_dis),R_Right(1)];
+    xx_cod_down=[R_Left(1),x_cod_down(i,1:n_dis),R_Right(1)];
+    yy_cod_up=[R_Left(2),y_cod_up(i,1:n_dis),R_Right(2)];
+    yy_cod_down=[R_Left(2),y_cod_down(i,1:n_dis),R_Right(2)];
+    plot(xx_cod_up,yy_cod_up,'r-',....
+    xx_cod_down,yy_cod_down,'r-','LineWidth',1)
+    end
+
+    hold off
+    legend('von Mises strain')
+    
+    saveas(gcf,'png/vonMisesStrain.png')
+    
 % disp_scale=10;
 % U=disp_scale*U;
 % V=disp_scale*V;
